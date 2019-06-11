7c8d3ba63cbea72b7aed765659b42ed5f4357eeb
MiTonder <48181821+MiTonder@users.noreply.github.com>
2019-06-11 17:20:27 +0200

    Update botstats.py

--- botstats/botstats.py
+++ botstats/botstats.py
@@ -41,7 +41,7 @@ class BotStats(commands.Cog):
         embed.add_field(name=f"Latency",value=f"The Latency For {self.bot.user.name} Is {self.bot.latency * 1000:.2f} MilliSeconds / {self.bot.latency:.3f} Seconds")
         embed.add_field(name=f"Important Information",value=f"Remember To :star: The [Repo](https://github.com/kyb3r/modmail) And Become A Patreon [Here](https://patreon.com/kyber)")
         embed.set_thumbnail(url=str(self.bot.user.avatar_url))
-        embed.set_footer(text=f"Anything missing? DM MiTonder#1792 with the suggestion.")
+        embed.set_footer(text=f"Stats missing? DM MiTonder#1792 with the suggestion.")
         embed.set_author(name=f"{self.bot.user.name} Stats")
 
         await ctx.send(embed=embed)
@@ -64,14 +64,14 @@ class BotStats(commands.Cog):
             description=f"**Here is the stats for {guild.name}, enjoy them:**",
 
         )
-        embed.add_field(name=f"Invite Link For {self.bot.user.name}",value=f"[Here](https://discordapp.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=268443792&scope=bot) Is The Invite Link For {self.bot.user.name}.")
-        embed.add_field(name="Bot User ID",value=f"`{self.bot.user.id}` Is The User ID For {self.bot.user.name}")
-        embed.add_field(name=f"Bot Prefix",value=f"The Prefix For {self.bot.user.name} Is `{self.bot.prefix}` Or {self.bot.user.mention}")
-        embed.add_field(name=f"Latency",value=f"The Latency For {self.bot.user.name} Is {self.bot.latency * 1000:.2f} MilliSeconds / {self.bot.latency:.3f} Seconds")
+        embed.add_field(name=f"Member Count",value=f"There Are {guild.member.count} Members In {guild.name}")
+        embed.add_field(name="Guild ID",value=f"`{guild.id}` Is The ID For {guild.name}")
+        embed.add_field(name=f"COMING SOON",value=f"The Prefix For {self.bot.user.name} Is `{self.bot.prefix}` Or {self.bot.user.mention}")
+        embed.add_field(name=f"COMING SOON",value=f"The Latency For {self.bot.user.name} Is {self.bot.latency * 1000:.2f} MilliSeconds / {self.bot.latency:.3f} Seconds")
         embed.add_field(name=f"Important Information",value=f"Remember To :star: The [Repo](https://github.com/kyb3r/modmail) And Become A Patreon [Here](https://patreon.com/kyber)")
-        embed.set_thumbnail(url=str(self.bot.user.avatar_url))
-        embed.set_footer(text=f"Anything missing? DM MiTonder#1792 with the suggestion.")
-        embed.set_author(name=f"{self.bot.user.name} Stats")
+        embed.set_thumbnail(url=str(url=str(ctx.guild.icon_url))
+        embed.set_footer(text=f"Stats missing? DM MiTonder#1792 with the suggestion.")
+        embed.set_author(name=f"{guild.name} Stats")
 
         await ctx.send(embed=embed)
         
