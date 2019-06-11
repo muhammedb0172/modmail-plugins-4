097b43dec078b656c55bba93fa4cf6ff0f04638e
MiTonder <48181821+MiTonder@users.noreply.github.com>
2019-06-11 15:38:13 +0200

    Update botstats.py

--- botstats/botstats.py
+++ botstats/botstats.py
@@ -41,8 +41,9 @@ class BotStats(commands.Cog):
         embed.add_field(name="Bot User ID",value=f"`{self.bot.user.id}` Is The User ID For {self.bot.user.name}")
         embed.add_field(name=f"Bot Prefix",value=f"The Prefix For {self.bot.user.name} Is `{self.bot.prefix}` Or {self.bot.user.mention}")
         embed.add_field(name=f"Latency",value=f"The Latency For {self.bot.user.name} Is {self.bot.latency * 1000:.2f} MilliSeconds / {self.bot.latency:.3f} Seconds")
+        embed.add_field(name=f"Important Information",value=f"Remember To :star: The [Repo](https://github.com/kyb3r/modmail) And Become A Patreon [Here](https://patreon.com/kyber)")
         embed.set_thumbnail(url=str(self.bot.user.avatar_url))
-        embed.set_footer(text=f'Remember To :star: The [Repo](https://github.com/kyb3r/modmail) And Become A Patreon [Here](https://patreon.com/kyber)')
+        embed.set_footer(text=f"Footer Text")
 
         await ctx.send(embed=embed)
 
