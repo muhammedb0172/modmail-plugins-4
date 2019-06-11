2afee104174edb8b9aacae27405ee53c96ed6447
MiTonder <48181821+MiTonder@users.noreply.github.com>
2019-06-11 17:27:01 +0200

    Update botstats.py

--- botstats/botstats.py
+++ botstats/botstats.py
@@ -41,7 +41,7 @@ class BotStats(commands.Cog):
         embed.add_field(name=f"Latency",value=f"The Latency For {self.bot.user.name} Is {self.bot.latency * 1000:.2f} MilliSeconds / {self.bot.latency:.3f} Seconds")
         embed.add_field(name=f"Important Information",value=f"Remember To :star: The [Repo](https://github.com/kyb3r/modmail) And Become A Patreon [Here](https://patreon.com/kyber)")
         embed.set_thumbnail(url=str(self.bot.user.avatar_url))
-        embed.set_footer(text=f"Stats missing? DM MiTonder#1792 with the suggestion.")
+        embed.set_footer(text=f"Stats missing? DM MiTonder#1792 with a suggestion for new stats")
         embed.set_author(name=f"{self.bot.user.name} Stats")
 
         await ctx.send(embed=embed)
@@ -70,7 +70,7 @@ class BotStats(commands.Cog):
         embed.add_field(name=f"COMING SOON",value=f"The Latency For {self.bot.user.name} Is {self.bot.latency * 1000:.2f} MilliSeconds / {self.bot.latency:.3f} Seconds")
         embed.add_field(name=f"Important Information",value=f"Remember To :star: The [Repo](https://github.com/kyb3r/modmail) And Become A Patreon [Here](https://patreon.com/kyber)")
         embed.set_thumbnail(url=str(ctx.guild.icon_url))
-        embed.set_footer(text=f"Stats missing? DM MiTonder#1792 with the suggestion")
+        embed.set_footer(text=f"Stats missing? DM MiTonder#1792 with a suggestion for new stats")
         embed.set_author(name=f"{guild.name} Stats")
 
         await ctx.send(embed=embed)
