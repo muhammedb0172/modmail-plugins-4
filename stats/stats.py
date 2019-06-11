503d6d976a6865b073f6901bb89aac5a76bc8ff0
MiTonder <48181821+MiTonder@users.noreply.github.com>
2019-06-11 17:40:19 +0200

    Update stats.py

--- stats/stats.py
+++ stats/stats.py
@@ -64,8 +64,8 @@ class Stats(commands.Cog):
             description=f"**Here is the stats for {ctx.guild.name}, enjoy them:**",
 
         )
-        embed.add_field(name=f"Member Count",value=f"There Are {Guild.member.count} Members In {ctx.guild.name}")
-        embed.add_field(name="Guild ID",value=f"`{Guild.id}` Is The ID For {ctx.guild.name}")
+        embed.add_field(name=f"Member Count",value=f"There Are {ctx.guild.member.count} Members In {ctx.guild.name}")
+        embed.add_field(name="Guild ID",value=f"`{ctx.guild.id}` Is The ID For {ctx.guild.name}")
         embed.add_field(name=f"COMING SOON",value=f"COMING SOON")
         embed.add_field(name=f"COMING SOON",value=f"COMING SOON")
         embed.add_field(name=f"Important Information",value=f"Remember To :star: The [Repo](https://github.com/kyb3r/modmail) And Become A Patreon [Here](https://patreon.com/kyber)")
