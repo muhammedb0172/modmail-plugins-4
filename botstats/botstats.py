2ed3cfad6fcbd1beafe1198607085bcc9c9e2d15
MiTonder <48181821+MiTonder@users.noreply.github.com>
2019-06-11 13:23:00 +0200

    Update botstats.py

--- botstats/botstats.py
+++ botstats/botstats.py
@@ -37,7 +37,16 @@ class BotInvite(commands.Cog):
             description=f"Here is the stats for {self.bot.user.name}, enjoy them.\n",
 
         )
-        embed.add_field(name="Invite link for the bot",value=f"[Here](https://discordapp.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=268443792&scope=bot) is the invite link for your bot.")
+        embed.add_field(name=f"Invite link for {self.bot.user.name}",value=f"[Here](https://discordapp.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=268443792&scope=bot) is the invite link for your bot.")
+        embed.add_field(name="Bot User ID",value=f"`{self.bot.user.id}` is the user id for {self.bot.user.name}")
+        embed.set_thumbnail(url=str(self.bot.user.avatar_url))
+
+        await ctx.send(embed=embed)
+
+def setup(bot):
+
+    bot.add_cog(BotInvite(bot))
+
         embed.set_thumbnail(url=str(self.bot.user.avatar_url))
 
         await ctx.send(embed=embed)
