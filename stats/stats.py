09e24174ffa9a78b9df25e09705e11c588c78096
MiTonder <48181821+MiTonder@users.noreply.github.com>
2019-06-13 09:05:39 +0200

    Update stats.py

--- stats/stats.py
+++ stats/stats.py
@@ -10,7 +10,7 @@ from core.models import PermissionLevel
 
 class Stats(commands.Cog):
 
-    """Get Useful Stats Directly In An Embed About Either The ModMail Bot Or The Server."""
+    """Get Useful Stats Directly In An Embed About Either The ModMail Bot, A User Or The Server."""
 
     
 
@@ -18,44 +18,19 @@ class Stats(commands.Cog):
 
         self.bot = bot
   
-    @commands.command()
-
+    @commands.group(name="stats", aliases=["stat"], invoke_without_command=True)
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def stats(self, ctx):
+        """Get A Cool Embed With A Lot Of Cool Stats About: Your ModMail, Server Or A User"""
 
-    async def botstats(self, ctx):
-
-        """Get A Neat Embed With Many Useful Stats About The ModMail Bot."""
-
-        embed = discord.Embed(
-
-            title=" ",
-
-            color=discord.Color.blue(),
-
-            description=f"**Here is the stats for {self.bot.user.name}, enjoy them:**",
-
-        )
-        embed.add_field(name=f"Invite Link For {self.bot.user.name}",value=f"[Here](https://discordapp.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=268443792&scope=bot) Is The Invite Link For {self.bot.user.name}.")
-        embed.add_field(name="Bot User ID",value=f"`{self.bot.user.id}` Is The User ID For {self.bot.user.name}")
-        embed.add_field(name=f"Bot Prefix",value=f"The Prefix For {self.bot.user.name} Is `{self.bot.prefix}` Or {self.bot.user.mention}")
-        embed.add_field(name=f"Latency",value=f"The Latency For {self.bot.user.name} Is {self.bot.latency * 1000:.2f} MilliSeconds / {self.bot.latency:.3f} Seconds")
-        embed.add_field(name="Bot Uptime",value=f"The Uptime For {self.bot.user.name} Is {self.bot.uptime}")
-        embed.add_field(name=f"Bot User Creation Date And Time",value=f"{self.bot.user.name} Was Created {self.bot.user.created_at:%A %d %B %Y} And The Time Was {self.bot.user.created_at:%H:%M:%S %p}")
-        embed.add_field(name=f"Important Information",value=f"Remember To :star: The [Repo](https://github.com/kyb3r/modmail) And Become A Patreon [Here](https://patreon.com/kyber)")
-        embed.set_thumbnail(url=str(self.bot.user.avatar_url))
-        embed.set_footer(text=f'If You Have Suggestions For More Stats, Please Use The Command "?suggestion" For More Info On How You Do It')
-        embed.set_author(name=f"{self.bot.user.name} Stats")
-
-        await ctx.send(embed=embed)
+        await ctx.send_help(ctx.command)
         
    
-    @commands.command(aliases=["server"])
-
+    @stats.command(name="server")
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-
-    async def serverstats(self, ctx):
-
+    async def server(self, ctx):
         """Get A Cool Embed With Many Useful Stats About Your Server"""
+
 
         embed = discord.Embed(
 
@@ -98,12 +73,37 @@ class Stats(commands.Cog):
 
         await ctx.send(embed=embed)
         
-        
-    @commands.command()
-
+    @stats.command(name=bot)
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def bot(self, ctx):
 
-    async def allstats(self, ctx):
+        """Get A Neat Embed With Many Useful Stats About The ModMail Bot."""
+
+        embed = discord.Embed(
+
+            title=" ",
+
+            color=discord.Color.blue(),
+
+            description=f"**Here is the stats for {self.bot.user.name}, enjoy them:**",
+
+        )
+        embed.add_field(name=f"Invite Link For {self.bot.user.name}",value=f"[Here](https://discordapp.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=268443792&scope=bot) Is The Invite Link For {self.bot.user.name}.")
+        embed.add_field(name="Bot User ID",value=f"`{self.bot.user.id}` Is The User ID For {self.bot.user.name}")
+        embed.add_field(name=f"Bot Prefix",value=f"The Prefix For {self.bot.user.name} Is `{self.bot.prefix}` Or {self.bot.user.mention}")
+        embed.add_field(name=f"Latency",value=f"The Latency For {self.bot.user.name} Is {self.bot.latency * 1000:.2f} MilliSeconds / {self.bot.latency:.3f} Seconds")
+        embed.add_field(name="Bot Uptime",value=f"The Uptime For {self.bot.user.name} Is {self.bot.uptime}")
+        embed.add_field(name=f"Bot User Creation Date And Time",value=f"{self.bot.user.name} Was Created {self.bot.user.created_at:%A %d %B %Y} And The Time Was {self.bot.user.created_at:%H:%M:%S %p}")
+        embed.add_field(name=f"Important Information",value=f"Remember To :star: The [Repo](https://github.com/kyb3r/modmail) And Become A Patreon [Here](https://patreon.com/kyber)")
+        embed.set_thumbnail(url=str(self.bot.user.avatar_url))
+        embed.set_footer(text=f'If You Have Suggestions For More Stats, Please Use The Command "?suggestion" For More Info On How You Do It')
+        embed.set_author(name=f"{self.bot.user.name} Stats")
+
+        await ctx.send(embed=embed)
+        
+    @stats.command(name=all)
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def all(self, ctx):
 
         """Sends 2 Embeds, Both `{prefix}serverstats` And `{prefix}botstats`"""
 
