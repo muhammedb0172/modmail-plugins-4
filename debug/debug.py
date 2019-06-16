402aeb18804da93caf7c11f800b30201cf10f2bb
MiTonder <48181821+MiTonder@users.noreply.github.com>
2019-06-16 12:06:50 +0200

    Update debug.py

--- debug/debug.py
+++ debug/debug.py
@@ -21,7 +21,7 @@ class Debug(commands.Cog):
     @commands.group(name="dbug", invoke_without_command=True)
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
     async def dbug(self, ctx):
-        """Send Debug Links Directly To Hastebin In Much Shorter Time Than Normal"""
+        """Send Debug Links Directly To Hastebin In Much Shorter Time Than Normal, Useful For People That Dont Know As Much About ModMail"""
 
         await ctx.send_help(ctx.command)
         
@@ -29,7 +29,7 @@ class Debug(commands.Cog):
     @dbug.command(name="start")
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
     async def start(self, ctx):
-        """Debug A Command"""
+        """Start A Debug Session, Do As The Bot Says, And Then It Will Work Perfectly"""
         
         timer = 15
 
@@ -38,11 +38,18 @@ class Debug(commands.Cog):
             description=f"Process Started, Deleting Cached Logs"
         ))
         
-        await asyncio.sleep(0,5)
+        await asyncio.sleep(0.5)
         
         await ctx.invoke(self.bot.get_command("debug wipe"))
         
         await asyncio.sleep(0.3)
+        
+        msg = await ctx.send(embed=discord.Embed(
+            color=discord.Color.blurple(),
+            description=f"Timer Starting In 3 Seconds, You Have 15 Seconds To Do The Command That Doesnt Work And Then You Will Get A Debug Link"
+        ))
+        
+        await asyncio.sleep(3)
          
         msg = await ctx.send(embed=discord.Embed(
             color=discord.Color.blurple(),
@@ -50,7 +57,9 @@ class Debug(commands.Cog):
         ))
         
         for i in range(timer):
-            await asyncio.sleep(1)
+            for i in range(10):
+                await asyncio.sleep(0.1)
+                
             timer = timer-1
             await msg.edit(embed=discord.Embed(
             color=discord.Color.blurple(),
