2365120a9203a37e09cd4b5f35c5a65f3950beb3
MiTonder <48181821+MiTonder@users.noreply.github.com>
2019-06-17 14:19:20 +0200

    Making a test plugin, please dont try this yet

---
+++ membercountgame/membercountgame.py
@@ -0,0 +1,30 @@
+import discord
+import asyncio
+from discord.ext import commands
+from core import checks
+from core.models import PermissionLevel
+
+#Cog = getattr(commands, 'Cog', object)
+
+class Gamemembercount(commands.Cog):
+
+    """Something"""
+
+    
+    def __init__(self, bot):
+
+        self.bot = bot
+  
+    @commands.command(name="dbug", invoke_without_command=True)
+    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
+    async def dbug(self, ctx):
+        """Let your modmail's game activity be changed to Listening to {ctx.guild.member_count} users"""
+
+        for i in range(6):
+            await ctx.invoke(self.bot.get_command(f"activity Listening to {ctx.guild.member_count} users"))
+            
+            await asyncio.sleep(10)
+            
+def setup(bot):
+
+    bot.add_cog(Gamemembercount(bot))
